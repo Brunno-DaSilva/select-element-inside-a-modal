cf633523716ddb8cb6674b607cd16712b58c8072
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-01-28 13:36:20 -0600

    finished the logic and add a settime out to display a msg based on the choise

--- app.js
+++ app.js
@@ -18,16 +18,35 @@ span.onclick = function () {
 };
 
 // Select element
-const modalContent = document.getElementById("modal-content");
+const moodAnswer = document.getElementById("mood-answer");
 const one = document.getElementById("one");
 const two = document.getElementById("two");
 const receiver = document.getElementById("receiver");
+const encouragement = document.getElementById("encouragement");
 const arrayEl = [];
-modalContent.addEventListener("click", function (event) {
+
+moodAnswer.addEventListener("click", function (event) {
   event = event || window.event;
   const target = event.target;
-  arrayEl.push(target);
+  const text = target.textContent;
+  arrayEl.push(text);
   console.log(arrayEl);
+  for (let i = 0; i < arrayEl.length; i++) {
+    if (arrayEl.length > 1) {
+      arrayEl.shift();
+    }
+    receiver.innerHTML = `<h2>${arrayEl[i]}</h2>`;
+  }
+  modal.style.display = "none";
+
+  setTimeout(() => {
+    if (arrayEl[0] === "🥱") {
+      encouragement.innerHTML = `<h2>Go get Some coffee</h2>`;
+    }
+    if (arrayEl[0] === "😎") {
+      encouragement.innerHTML = `<h2>You are awesome</h2>`;
+    }
+  }, 1000);
 });
 
 // When the user clicks anywhere outside of the modal, close it
