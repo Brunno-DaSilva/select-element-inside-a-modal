3735140d4c6354efe74908cc3f56e5f0880d53fa
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-01-28 14:14:16 -0600

    added comments to JS

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-// Get the modal
+/*
+################
+               Modal
+################
+*/
 const modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 const btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function () {
   modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
 };
 
-// Select element
+/*
+################
+               Select element & Display MSG
+################
+*/
+
 const moodAnswer = document.getElementById("mood-answer");
 const one = document.getElementById("one");
 const two = document.getElementById("two");
@@ -49,7 +51,13 @@ moodAnswer.addEventListener("click", function (event) {
   }, 500);
 });
 
-// When the user clicks anywhere outside of the modal, close it
+/*
+################
+               When the user clicks anywhere outside of 
+               the modal, close it
+################
+*/
+
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
